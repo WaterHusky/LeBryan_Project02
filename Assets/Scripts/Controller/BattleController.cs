0b15ddc03b459f107769f61075c111b632593850
Bryan Le <bryan.le7826@gmail.com>
2022-11-05 21:00:50 -0600

    Created Statemachines for each phase

---
+++ Assets/Scripts/Controller/BattleController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleController : StateMachine
+{
+    public CameraRig cameraRig;
+    public Board board;
+    public LevelData levelData;
+    public Transform tileSelectionIndicator;
+    public Point pos;
+    void Start()
+    {
+        ChangeState<InitBattleState>();
+    }
+}
