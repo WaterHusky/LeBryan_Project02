e397490244689b14fa18fa3c1febc63cf721d104
Bryan Le <bryan.le7826@gmail.com>
2022-11-30 10:59:30 -0600

    Redid abilities and clean up some graphical issues

---
+++ Assets/Scripts/Tactical/Actor/Component/Ability/EffectTarget/EnemyAbilityEffectTarget.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Tactical.Actor.Component;
+using Tactical.Grid.Component;
+using Tactical.Core.Enums;
+
+public class EnemyAbilityEffectTarget : AbilityEffectTarget 
+{
+	Alliance alliance;
+
+	void Start ()
+	{
+		alliance = GetComponentInParent<Alliance>();
+	}
+
+	public override bool IsTarget (Tile tile)
+	{
+		if (tile == null || tile.content == null)
+			return false;
+
+		Alliance other = tile.content.GetComponentInChildren<Alliance>();
+		return alliance.IsMatch(other, Targets.Foe);
+	}
+
+}
